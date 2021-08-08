029642950336c67bf8d5c2eefe0403403dc5aaeb
David Morales <morales.david1997@gmail.com>
2021-08-07 23:25:39 -0400

    chore(api): :building_construction: refactor server management process to external file

--- nautilus-api/app.js
+++ nautilus-api/app.js
@@ -6,8 +6,5 @@ app.get('/', (req, res) => {
     res.json('Nautilus-API is working...');
 });
 
-const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
-    console.log(`Nautilus-API Listening on http://localhost:${PORT}`);
-});
+module.exports = app;
---
+++ nautilus-api/config/server.config.js
@@ -0,0 +1,6 @@
+const config = {
+    port: process.env.PORT || 8080,
+    host: process.env.HOST || 'localhost'
+};
+
+module.exports = config;
